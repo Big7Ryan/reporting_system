2e056d2f71b1b0616abc7e285a3778e633adf1f4
Big7Ryan <32688996+Big7Ryan@users.noreply.github.com>
2020-09-24 03:56:06 -0500

    Add files via upload

---
+++ src/main/java/com/antra/evaluation/reporting_system/service/ExcelService.java
@@ -0,0 +1,18 @@
+package com.antra.evaluation.reporting_system.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import com.antra.evaluation.reporting_system.pojo.report.ExcelData;
+import com.antra.evaluation.reporting_system.pojo.report.ExcelFile;
+
+public interface ExcelService {
+    InputStream getExcelBodyById(String id);
+    File generateExcelReport(ExcelData data) throws IOException;
+    List<ExcelFile> getExcelFiles();
+    void saveExcelFiles(ExcelFile file);
+	String createId();
+	void deleteExcelBodyById(String id); 
+}
